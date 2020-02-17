70b7d86362c97c2b29549d62114b3fea1e6feb84
KradasA4 <wichai.sw19@gmail.com>
2020-02-17 11:14:19 +0700

    add exercise 1 & 2

--- week3_javascript/index.js
+++ week3_javascript/index.js
@@ -1,9 +1,24 @@
+//Lab1
 let human;
 const name = "Wichai";
 human = name;
 console.log(human);
 
+//Lab2
 let myWalletValue = 200;
 let myParentsName = "father & mother";
 let myAddress = "134 Suanson Village";
 let ourUniverseAge = 13800000000;
+
+
+// Exercise1
+console.log(`hello ${1}`);
+console.log(`hello ${"name"}`);
+console.log(`hello ${name}`);
+
+// Exercise2
+let myName = "Wichai"
+let myAge = "25"
+let Address = "134 Suanson Village";
+let myProfile = `My name is ${myName}. I'm ${myAge} years old.\nI live at ${Address}`
+console.log(myProfile)
