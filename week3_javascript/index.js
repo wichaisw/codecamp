cddcb518fc1eefc7674c52f111807dba262263e0
KradasA4 <wichai.sw19@gmail.com>
2020-02-17 10:42:03 +0700

    add JavaScript Lab2

--- week3_javascript/index.js
+++ week3_javascript/index.js
@@ -2,3 +2,8 @@ let human;
 const name = "Wichai";
 human = name;
 console.log(human);
+
+let myWalletValue = 200;
+let myParentsName = "father & mother";
+let myAddress = "134 Suanson Village";
+let ourUniverseAge = 13800000000;
