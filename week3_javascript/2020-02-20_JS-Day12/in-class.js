0acada3b3439f8241c03d8c392643d1499f72a8e
KradasA4 <wichai.sw19@gmail.com>
2020-02-21 01:24:41 +0700

    turn each exercise into a  function | fix js exercise4 bug
    prevent the loop to end when num is empty string

--- week3_javascript/2020-02-20_JS-Day12/in-class.js
+++ week3_javascript/2020-02-20_JS-Day12/in-class.js
@@ -1,137 +1,144 @@
-// Arrow Function Exercise 1
-let ask = (question, yes, no) => {
-    if (confirm(question)) yes()
-    else no();
-}
+// February 20, 2020: Day 12 JavaScript in-class labs
+// ผมเก็บ exercise แต่ละข้อไว้ในฟังก์ชันอีกที จะได้เลือกรันดูเป็นข้อๆ ได้ในเบราเซอร์
+// หวังว่าจะตรวจง่ายขึ้น
 
-ask(
-    "Do you agree?",
-    () => {alert("You agreed.");},
-    () => {alert("You canceled the execution");}
-); 
+
+// Arrow Function Exercise 1
+
+let arrowFunctionExercise1 = () => {
+    let ask = (question, yes, no) => {
+        if (confirm(question)) yes();
+        else no();
+    };
+
+    ask(
+        "Do you agree?",
+        () => {alert("You agreed.");},
+        () => {alert("You canceled the execution");}
+    ); 
+
+};
+// arrowFunctionExercise1();
+
 
 // Clean Code Exercise 1
-// แก้ไขการเขียนโค้ดต่อไปนี้
-// function pow(x,n)
-// {
-//   let result=1;
-//   for(let i=0;i<n;i++) {result*=x;}
-//   return result;
-// }
-// let x=prompt("x?",''), n=prompt("n?",'')
-// if (n<=0)
-// {
-//   alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
-// }
-// else
-// {
-//   alert(pow(x,n))
-// }
-
-function pow(x, n) {
-  let result = 1;
-  for (let i = 0; i < n; i++) result *= x;
+/* แก้ไขการเขียนโค้ดต่อไปนี้
+function pow(x,n)
+{
+  let result=1;
+  for(let i=0;i<n;i++) {result*=x;}
   return result;
 }
-
-let x = prompt("x?", ''), n = prompt("n?", '');
-
-if (n <= 0) {
+let x=prompt("x?",''), n=prompt("n?",'')
+if (n<=0)
+{
   alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
-} else {
-  alert( pow(x, n) );
-} 
+}
+else
+{
+  alert(pow(x,n))
+}
+*/
+
+let CleanCodeExercise1 = () => {
+
+    function pow(x, n) {
+    let result = 1;
+    for (let i = 0; i < n; i++) result *= x;
+    return result;
+    };
+
+    let x = prompt("x?", ''), n = prompt("n?", '');
+
+    if (n <= 0) {
+    alert(`Power ${n} is not supported, please enter an integer number greater than zero`);
+    } else {
+    alert( pow(x, n) );
+    } ;
+
+};
+// CleanCodeExercise1();
+
 
 // JavaScript Exercise 1: BMI#1 
-let user1 = prompt("Enter 1st person name.");
-let bmi1 = Number(prompt("Enter 1st person bmi"));
-let user2 = prompt("Enter 2nd person name.");
-let bmi2 = Number(prompt("Enter 2nd person bmi"));
 
+let jsExercise1_BMI = () => {
 
-let bmiCompare = (a, b) => {
-    if (a > b) {
-        alert(`${user1} has more BMI than ${user2}
-        ${user1} BMI is ${bmi1}`);
-    } else if(a > b) {
-        alert(`${user2} has more BMI than ${user1}
-        ${user2} BMI is ${bmi2}`);
-    } else {
+    let user1 = prompt("Enter 1st person name.");
+    let bmi1 = Number(prompt("Enter 1st person bmi"));
+    let user2 = prompt("Enter 2nd person name.");
+    let bmi2 = Number(prompt("Enter 2nd person bmi"));
+
+    if (bmi1 > bmi2) {
+        alert(`${user1} has more BMI than ${user2}\n${user1}'s BMI is ${bmi1}`);
+    } else if(bmi2 > bmi1) {
+        alert(`${user2} has more BMI than ${user1}\n${user2}'s BMI is ${bmi2}`);
+    } else if (bmi1 === bmi2 ) {
         alert(`${user1} and ${user2} has equal BMI`);
-    }
-}
-
-bmiCompare(bmi1, bmi2); 
-
-
-let bmiCompare = (a, b) => {
-    if (a > b) {
-        alert(`${user1} has more BMI than ${user2}
-        ${user1} BMI is ` + a);
-    } else if(a > b) {
-        alert(`${user2} has more BMI than ${user1}
-        ${user2} BMI is ` + b);
     } else {
-        alert(`${user1} and ${user2} has equal BMI`);
-    }
-}
+        alert(`BMI must be number only`);
+    };
 
-bmiCompare(bmi1, bmi2);
+};
+// jsExercise1_BMI();
 
 
 // JavaScript Exercise 2: BMI#2
-let user1 = prompt("Enter 1st person name.");
-let cmHeight1 = Number(prompt("Enter 1st person height (cm.)"));
-let weight1 = Number(prompt("Enter 1st person weight (kg.)"));
 
-let user2 = prompt("Enter 2nd person name.");
-let cmHeight2 = Number(prompt("Enter 2nd person height (cm.)"));
-let weight2 = Number(prompt("Enter 2nd person weight (kg.)"));
+let jsExercise2_BMI2 = () => {
 
+    let user1 = prompt("Enter 1st person name.");
+    let cmHeight1 = Number(prompt("Enter 1st person height (cm.)"));
+    let weight1 = Number(prompt("Enter 1st person weight (kg.)"));
 
-// คำนวณค่า BMI จาก ส่วนสูง(เซนติเมตร) และน้ำหนัก (กิโลกรัม)
-let bmiCalculator = (cmHeight, weight) => {
-    let mHeight = cmHeight / 100;
-    let bmi = weight / mHeight**2;
-    return bmi;
-}
+    let user2 = prompt("Enter 2nd person name.");
+    let cmHeight2 = Number(prompt("Enter 2nd person height (cm.)"));
+    let weight2 = Number(prompt("Enter 2nd person weight (kg.)"));
 
-function bmiCalculator (cmHeight, weight) {
-    let mHeight = cmHeight / 100;
-    let bmi = weight / mHeight**2;
-    return bmi;
-}
+    function bmiCalculator (cmHeight, weight) {
+        let mHeight = cmHeight / 100;
+        let bmi = weight / mHeight**2;
+        return bmi;
+    }
 
-let bmi1 = bmiCalculator(cmHeight1, weight1)
-let bmi2 = bmiCalculator(cmHeight2, weight2)
+    let bmi1 = bmiCalculator(cmHeight1, weight1);
+    let bmi2 = bmiCalculator(cmHeight2, weight2);
 
-if (bmi1 > bmi2) {
-    alert(`${user1} has more BMI than ${user2} \n${user1} BMI is ${bmi1}`);
-} else if(bmi2 > bmi1) {
-    alert(`${user2} has more BMI than ${user1} \n${user2} BMI is ${bmi2}`);
-} else {
-    alert(`${user1} and ${user2} has equal BMI`);
-}
+    if (bmi1 > bmi2) {
+        alert(`${user1} has more BMI than ${user2} \n${user1}'s BMI is ${bmi1}`);
+    } else if(bmi2 > bmi1) {
+        alert(`${user2} has more BMI than ${user1} \n${user2}'s BMI is ${bmi2}`);
+    } else {
+        alert(`${user1} and ${user2} has equal BMI`);
+    };
 
+};
+// jsExercise2_BMI2();
 
 // JavaScript Exercise 3: Highest Number
-let highestNum = -Infinity;
-while (true) {
-    let num = prompt("Please enter number.");
-    
-    if ( isNaN(num) ) {
-        alert("Please enter only number.")
-        continue;
-    } else if (num === null && highestNum !== -Infinity) {
-        alert(`The highest number is ${highestNum}`);
-        break;
-    } else if (Number(num) > Number(highestNum) && num !== null) {
-        highestNum = num;
-    } else if (highestNum === -Infinity) {
-        alert("Please enter some number.")
-        continue; 
+
+let jsExercise3_highestNumber = () => {
+
+    let highestNum = -Infinity;
+    while (true) {
+        let num = prompt("Please enter number.");
+        
+        if ( isNaN(num) ) {
+            alert("Please enter only number.")
+            continue;
+        } else if (num === null && highestNum !== -Infinity) {
+            alert(`The highest number is ${highestNum}`);
+            break;
+        } else if (Number(num) > Number(highestNum) && num !== null) {
+            highestNum = num;
+        } else if (highestNum === -Infinity) {
+            alert("Please enter some number.");
+            continue; 
+        };
     };
+
 };
+// jsExercise3_highestNumber();
 
 // JavaScript Exercise 4: 2 Highest Number
 /* ทำเหมือนเดิม กำหนดให้หาก num ที่กรอกมากกว่า highestNum1 ก็เก็บค่านั้นไว้ใน highestNum1
@@ -148,33 +155,36 @@ while (true) {
  * หากไม่ดักไว้จะทำให้ติด loop ไม่จบถ้าเลขตัวที่สองน้อยกว่าเลขตัวแรก
 */
 
-let highestNum1 = -Infinity;
-let highestNum2 = -Infinity;;
-while (true) {
-    let num = prompt("Please enter number.");
-    
-    if ( isNaN(num) ) {
-        alert("Please enter only number.")
-        continue;
-    } else if (!num && highestNum1 !== -Infinity && highestNum2 !== -Infinity) {
-        alert(`The highest number is ${highestNum1}.\nThe second highest number is ${highestNum2}.`);
-        break;
-    } else if (Number(num) > Number(highestNum1) && num !== null && num !== "") {
-        let tempNum = highestNum1;
-        highestNum1 = num;
-        highestNum2 = tempNum;
+let jsExercise4_highestNumber2 = () => {
 
-        if (highestNum2 === -Infinity || highestNum2 === "") {
-            alert("Please enter at least one more number.")
+    let highestNum1 = -Infinity;
+    let highestNum2 = -Infinity;;
+    while (true) {
+        let num = prompt("Please enter number.");
+        
+        if ( isNaN(num) ) {
+            alert("Please enter only number.")
+            continue;
+        } else if (!num && num != "" && highestNum1 !== -Infinity && highestNum2 !== -Infinity) {
+            alert(`The highest number is ${highestNum1}.\nThe second highest number is ${highestNum2}.`);
+            break;
+        } else if (Number(num) > Number(highestNum1) && num !== null && num !== "") {
+            let tempNum = highestNum1;
+            highestNum1 = num;
+            highestNum2 = tempNum;
+
+            if (highestNum2 === -Infinity || highestNum2 === "") {
+                alert("Please enter at least one more number.");
+                continue; 
+            };
+
+        } else if (Number(num) <= Number(highestNum1) && Number(num) > Number(highestNum2) && num !== null && num !== "") {
+            highestNum2 = num;
+        } else if (highestNum1 === -Infinity || num === "") {
+            alert("Please enter some number.");
             continue; 
         };
-
-    } else if (Number(num) <= Number(highestNum1) && Number(num) > Number(highestNum2) && num !== null && num !== "") {
-        highestNum2 = num;
-    } else if (highestNum1 === -Infinity || num === "") {
-        alert("Please enter some number.")
-        continue; 
     };
+
 };
-
-
+jsExercise4_highestNumber2();
