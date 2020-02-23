3d66e7e877173858e8d61106dbb773038e29e058
KradasA4 <wichai.sw19@gmail.com>
2020-02-24 03:22:12 +0700

    fix duplicated feed text issue

--- jquery-hw/script.js
+++ jquery-hw/script.js
@@ -8,14 +8,23 @@
 //   });
 // });
 
-//normal append way
+//normal append way *template
+// $(document).ready(function() {
+//   $(".post-button").click(function() {
+//     let postText = $("#post-text").val();
+//     let feedTemplate = $(".fb-feed-template").html();
+    
+//     $(".fb-feed-wrapper").append(feedTemplate);
+//     $(".received-post").append(postText);
+//   });
+// });
+
+//normal append way *minify html
 $(document).ready(function() {
   $(".post-button").click(function() {
     let postText = $("#post-text").val();
-    let feedTemplate = $(".fb-feed-template").html();
     
-    $(".fb-feed-wrapper").append(feedTemplate);
-    $(".received-post").append(postText);
+    $(".fb-feed-wrapper").append(`<div class="fb-feed-template"> <div class="fb-feed-layout"> <div class="fb-post-padding"> <img src="" alt=""> <span class="username">username</span> </div><div class="feed-content"> <div class="received-post fb-post-padding"> ${postText} </div><div class="fb-post-padding"> image </div></div><div class="feed-engagement fb-post-padding"> <button>ถูกใจ</button> </div><div class="fb-post-padding"> comment </div></div>`);
   });
 });
 
