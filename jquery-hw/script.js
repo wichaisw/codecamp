b3652606b37ac312106ae7c1a8cc66b07be8e7f8
KradasA4 <wichai.sw19@gmail.com>
2020-02-24 03:06:44 +0700

    add jQuery to facebook post
    post button is working but there's a duplicate issue since jQuery keep repeating adding feedTemplate to the same calss

--- jquery-hw/script.js
+++ jquery-hw/script.js
@@ -1,3 +1,39 @@
+// append template method (hidden)
+
+//load way
+// $(document).ready(function() {
+//   $(".post-button").click(function() {
+//     let postText = $("#post-text").val();
+//     $(".fb-feed-wrapper").load("./feed.html");
+//   });
+// });
+
+//normal append way
+$(document).ready(function() {
+  $(".post-button").click(function() {
+    let postText = $("#post-text").val();
+    let feedTemplate = $(".fb-feed-template").html();
+    
+    $(".fb-feed-wrapper").append(feedTemplate);
+    $(".received-post").append(postText);
+  });
+});
+
+//clone way
+// $(document).ready(function() {
+//   $(".post-button").click(function() {
+//     let postText = $("#post-text").val();
+//     $(".fb-feed-wrapper").append(function() {
+//     $(".fb-post-content").clone().appendTo(".fb-feed-wrapper")
+//     });
+//   });
+// });
+
+
+
+
+
+
 // $(document).ready(function() {
 //     $("#post-text").autoResize();
 // });
