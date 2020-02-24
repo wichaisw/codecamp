7707feb31e9f70c269087b56fe33c2354e6ee912
KradasA4 <wichai.sw19@gmail.com>
2020-02-24 15:52:43 +0700

    add exrcises

--- jquery-hw/script.js
+++ jquery-hw/script.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
   $(".post-button").click(function() {
     let attachmentURL = $("#image-video-attach-url").val();
     let postText = $("#post-text").val();
-    $(".fb-feed-wrapper").append(`<div class="fb-feed-template"> <div class="fb-feed-layout"> <div class="fb-post-padding"> <img src="" alt=""> <span class="username">username</span> </div><div class="feed-content"> <div class="received-post fb-post-padding"> ${postText} </div><div class="received-attachment fb-post-padding"> <img src="${attachmentURL}"></div></div><div class="feed-engagement fb-post-padding"> <button>ถูกใจ</button> </div><div class="fb-post-padding"> comment </div></div></div>`);
+    $(".fb-feed-wrapper").append(`<div class="fb-feed-template">
+    <div class="fb-feed-layout"> <div class="fb-post-padding"> <img src="" alt=""> <span class="username">username</span> </div><div class="feed-content"> <div class="received-post fb-post-padding"> ${postText} </div><div class="received-attachment fb-post-padding"> <img src="${attachmentURL}"></div></div><div class="feed-engagement fb-post-padding"> <button>ถูกใจ</button> </div><div class="fb-post-padding"> comment </div></div></div>`);
     
   });
 });
---
+++ week3_javascript/2020-02-24_JS-Day14/lab.js
@@ -0,0 +1,118 @@
+// February 24, 2020: Day 14 JavaScript in-class labs
+// lab1
+const obj = {
+  name: "Wichai",
+  age: 25,
+  height: 170,
+  weight: 62
+};
+
+let newObj = {};
+for (key in obj) {
+  newObj[key] = obj[key]
+};
+
+console.log(newObj)
+
+newObj["name"] = "Sonter";
+console.log(newObj)
+console.log(obj)
+
+// lab22
+
+const obj = {
+  name: "sonter",
+  age: 18,
+  height: 196,
+  weight: 70
+};
+
+const obj2 = {
+  name: "Nat",
+  weight: 98
+};
+
+const obj3 = {
+  name: "Tle",
+  skill: "nodejs"
+};
+;
+Object.assign(obj3, obj, obj2)
+
+// obj3 = {name: "Nat", skill: "nodejs", age: 18, height: 196, weight: 98}
+
+// Exercise 4.18: ข้อ 1
+/* 
+ * ให้ทำตามคำสั่งต่อไปนี้
+ * สร้าง Object เปล่าขึ้นมา
+ * เพิ่ม properties name เข้าไปและให้ value เป็น “Sonter”
+ * เพิ่ม properties surname เข้าไปและให้ value เป็น “Pakorn”
+ * เปลี่ยน properties name เป็น “Boy”
+ * ลบ properties name ออกจาก Object
+*/
+const obj = {};
+
+obj.name = "Sonter";
+obj.surname = "Pakorn";
+obj.name = "Boy";
+
+delete obj.name
+
+
+// Exercise 4.18: ข้อ 2
+// สร้างฟังก์ชันเช็กว่า object นั้น empty ไหม ถ้าไม่มี properties ให้ return true, ถ้ามี return false
+function isEmpty(obj) {
+  let isEmpty = true;
+  for (let key in obj) {
+    isEmpty = false;
+  };
+  return isEmpty;
+};
+
+// Exercise 4.18: ข้อ 3
+/* 
+ * const user = {
+ *   name: "John"
+ * };
+ * 
+ * // does it work?
+ * user.name = "Pete";
+*/
+
+// ไม่ error
+
+// Exercise 4.18: ข้อ 4
+//จงเขียนฟังก์ชัน sum(obj) ที่รับ obj ที่เก็บ properties โดยมี key เป็นชื่อพนักงานและมี value เป็นเงินเดือน ให้ฟังก์ชันคืนค่าเป็นผลรวมของเงินเดือนพนักงานทั้งหมด
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+
+function sum(obj) {
+  let total = 0;
+  for (let key in obj) {
+    total = total + obj[key];
+  };
+  return total;
+};
+
+sum(salaries); // return 390
+
+// Exercise 4.18: ข้อ 5
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumeric(obj, times) {
+  for (let key in obj) {
+    if (typeof (obj[key]) == "number") {
+      obj[key] = obj[key] * times;
+    };
+  };
+  return obj;
+};
+
+multiplyNumeric(menu, 2)
