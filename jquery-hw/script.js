027c0c8d5ba3b839e83b03dc2650027dbdc4d570
KradasA4 <wichai.sw19@gmail.com>
2020-02-27 01:09:00 +0700

    fix attachment adding bug | prettify css | change feed append to prepend

--- jquery-hw/script.js
+++ jquery-hw/script.js
@@ -1,19 +1,44 @@
 //normal append way *minify html
-$(document).ready(function() {
-  
-  $("#image-video-attach-btn").click(function() {
-    let attachmentURL = $("#image-video-attach-url").val();
-    $(".img-attachment").attr("src", `${attachmentURL}`);
-  });
-  
-  $(".post-button").click(function() {
-    let attachmentURL = $("#image-video-attach-url").val();
-    let postText = $("#post-text").val();
-    $(".fb-feed-wrapper").append(`<div class="fb-feed-template">
-    <div class="fb-feed-layout"> <div class="fb-post-padding"> <img src="" alt=""> <span class="username">username</span> </div><div class="feed-content"> <div class="received-post fb-post-padding"> ${postText} </div><div class="received-attachment fb-post-padding"> <img src="${attachmentURL}"></div></div><div class="feed-engagement fb-post-padding"> <button>ถูกใจ</button> </div><div class="fb-post-padding"> comment </div></div></div>`);
-  });
-});
 
+
+
+$(document).ready(function() {
+  //click 'Photo' button to preview an image
+  $("#image-video-attach-btn").click(function() {
+    let attachmentUrl = $("#image-video-attach-url").val();
+    $(".img-attachment").attr("src", `${attachmentUrl}`);
+    if (attachmentUrl !== "") {
+      $(".img-attachment").css({display: "block"});
+    };
+  });
+
+  //preview an image after form focus is out too
+  $("#image-video-attach-url").blur(function() {
+    $(".img-attachment").attr("src", `${$(this).val()}`);
+    if ($(this).val() !== "") {
+      $(".img-attachment").css({display: "block"});
+    };
+  });
+
+  // add feed if #post-text is not empty
+  $(".post-button").click(function() {
+    let attachmentUrl = $("#image-video-attach-url").val();
+    let postText = $("#post-text").val();
+    let htmlFeed = `<div class="fb-feed-template"> <div class="fb-feed-layout"> <div class="feed-header-grid fb-post-padding"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <span class="username">username</span> </div> <div class="feed-content"> <div class="received-post">${postText}</div> <div class="received-attachment"> <img src="${attachmentUrl}" class="feed-img-attachment"> </div> </div> <div class="feed-engagement fb-post-padding"> <button class="like-button">Like</button> </div> <div class="fb-post-padding"> comment </div> </div> </div>`
+
+    if (postText !== "" || attachmentUrl !== "") {
+      $(".fb-feed-wrapper").css({display: "flex"});
+      $(".fb-feed-wrapper").prepend(htmlFeed);
+
+      $("#post-text").val("");
+      $("#image-video-attach-url").val("");
+      $(".img-attachment").attr("src", "");
+    } else {
+      alert("You say it best,\nwhen you say nothing at all.");
+    };
+  });
+
+});
 
 
 // append template method (hidden)
