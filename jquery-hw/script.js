8df3e55fa8c14567bac6e0da8d24442480f913fb
KradasA4 <wichai.sw19@gmail.com>
2020-02-27 11:41:11 +0700

    fix attachment form issue | add key 13 down event to attachment form
    fix issue where attachment form refresh page after press enter button.

--- jquery-hw/script.js
+++ jquery-hw/script.js
@@ -4,10 +4,13 @@
 
 $(document).ready(function() {
   // define username
-  let nickname = prompt("What's your nickname?")
-
+  let nickname = prompt("What's your nickname?");
+  while(nickname == "" || nickname == null) {
+    nickname = prompt("What's your nickname?");
+  };
 
   //click 'Photo' button to preview an image
+  
   $("#image-video-attach-btn").click(function() {
     let attachmentUrl = $("#image-video-attach-url").val();
     $(".img-attachment").attr("src", `${attachmentUrl}`);
@@ -16,7 +19,17 @@ $(document).ready(function() {
     };
   });
 
+
   //preview an image after mouse leave the form too
+  $("#image-video-attach-url").keydown(function(e) {
+    if (e.which == 13) {
+      $(".img-attachment").attr("src", `${$(this).val()}`);
+      if ($(this).val() !== "") {
+      $(".img-attachment").css({display: "block"});
+      };
+    };
+  });
+  
   $("#image-video-attach-url").mouseleave(function() {
     $(".img-attachment").attr("src", `${$(this).val()}`);
     if ($(this).val() !== "") {
@@ -24,16 +37,20 @@ $(document).ready(function() {
     };
   });
 
-  // add feed if #post-text is not empty
+  // add feed if #post-text iss not empty
   $(".post-button").click(function() {
     let timeStamp = new Date();
     
 
     let attachmentUrl = $("#image-video-attach-url").val();
     let postText = $("#post-text").val();
+
+
+
     let htmlFeed = `<div class="fb-feed-template"> <div class="fb-feed-layout"> <div class="feed-header-grid fb-post-padding"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <div class="username-box"> <b>${nickname}</b> <br> ${timeStamp.toLocaleString()} </div> </div> <div class="feed-content"> <div class="received-post">${postText}</div> <div class="received-attachment"> <img src="${attachmentUrl}" class="feed-img-attachment"> </div> </div> <div class="feed-engagement fb-post-padding"> <button class="like-button">Like</button> </div> <div class="fb-post-padding"> comment </div> </div> </div>`
 
     if (postText !== "" || attachmentUrl !== "") {
+      
       $(".fb-feed-wrapper").css({display: "flex"});
       $(".fb-feed-wrapper").prepend(htmlFeed);
 
