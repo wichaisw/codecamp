4c3624ded7b8cf06b1b49da54c1e9897145da9b5
KradasA4 <wichai.sw19@gmail.com>
2020-02-29 21:53:05 +0700

    add reply button
    the reply button is an alternative way to reply comment apart from pressing enter

--- jquery-hw/script.js
+++ jquery-hw/script.js
@@ -1,7 +1,5 @@
 //normal append way *minify html
 
-
-
 $(document).ready(function() {
   // define username
   let nickname = prompt("What's your nickname?");
@@ -44,13 +42,19 @@ $(document).ready(function() {
     }
   });
     
-    // add feed if #post-text iss not empty
+
+  $("#post-text").on('input', function() {
+    var scroll_height = $("#post-text").get(0).scrollHeight;
+  
+    $("#post-text").css('height', scroll_height + 'px');
+  });
+
+  // add feed if #post-text is not empty
   $(".post-button").click(function() {
     let timeStamp = new Date();
     let attachmentUrl = $("#image-video-attach-url").val();
     let postText = $("#post-text").val();
-
-    let htmlFeed = `<div class="fb-feed-template"> <div class="fb-feed-layout"> <div class="feed-header-grid fb-post-padding"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <div class="username-box"> <span class="username">${nickname}</span> <br> ${timeStamp.toLocaleString()} </div> </div> <div class="feed-content"> <div class="received-post">${postText}</div> <div class="received-attachment"> <img src="${attachmentUrl}" class="feed-img-attachment"> </div> </div> <div class="feed-engagement fb-post-padding"> <button class="like-button">Like</button> <span></span> </div> <div class="fb-post-padding"> <form class="fb-comment-grid"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <label for="comment-text"> <textarea name="comment-text" class="comment-text" rows="2" placeholder="Write a comment..."></textarea> </label> </form> </div> </div> </div>`
+    let htmlFeed = `<div class="fb-feed-template"> <div class="fb-feed-layout"> <div class="feed-header-grid fb-post-padding"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <div class="username-box"> <span class="username">${nickname}</span> <br> ${timeStamp.toLocaleString()} </div> </div> <div class="feed-content"> <div class="received-post">${postText}</div> <div class="received-attachment"> <img src="${attachmentUrl}" class="feed-img-attachment"> </div> </div> <div class="feed-engagement fb-post-padding"> <button class="like-button">Like</button> <span></span> </div> <div class="fb-post-padding"> <form action="#" class="fb-comment-grid"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <label for="comment-text"> <textarea name="comment-text" class="comment-text" rows="2" placeholder="Write a comment..."></textarea> </label> <button class="comment-send-button"> reply </button> </form> </div> </div> </div>`
 
     if (postText !== "" || attachmentUrl !== "") {
       $(".fb-feed-wrapper").css({display: "flex"});
@@ -73,10 +77,10 @@ $(document).ready(function() {
     $(this).toggleClass("blue-liked-button");
     if ($(this).attr("class") == "like-button blue-liked-button") {
       totalLike = totalLike + 1;
-      likeCounterDisplay.html(`${totalLike}`);
+      likeCounterDisplay.html(`${totalLike} liked`);
     } else if ($(this).attr("class") == "like-button") {
       totalLike = totalLike - 1;
-      likeCounterDisplay.html(`${totalLike}`);
+      likeCounterDisplay.html(`${totalLike} liked`);
     };
 
     if (totalLike == 0) {
@@ -86,27 +90,37 @@ $(document).ready(function() {
     }
   });
 
-  // press enter to reply
+  // press enter to reply, press shift+enter to break a line
+  let timeStamp = new Date();
   $(".fb-feed-wrapper").on("keydown", ".comment-text", function(e) {
     let comment = $(this).val()
-    let timeStamp = new Date();
-
-      if (e.which == 13 && e.shiftKey) {
-        $(this).text("\n")
-      }
-      
-      if (e.which == 13 && !e.shiftKey) {
-        e.preventDefault();
-        if (comment !== "" && comment !== "\n") {
-          $(this).parent().children().last().after(`<div class="replied-grid"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <div class="comment-content"> <div><span class="username">${nickname}</span> ${comment}</div> <span class="feed-timestamp">${timeStamp.toLocaleString()}</span> </div> </div>`);
-        } else {
-          alert("You say it best,\nwhen you say nothing at all.");
-        };
-
-        $(this).val("");
-      } 
+  
+    if (e.which == 13 && e.shiftKey) {
+      $(this).text("\n")
     }
-  );
+    
+    if (e.which == 13 && !e.shiftKey) {
+      e.preventDefault();
+      if (comment !== "" && comment !== "\n") {
+        $(this).parent().children().last().after(`<div class="replied-grid"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <div class="comment-content"> <div><span class="username">${nickname}</span> ${comment}</div> <span class="feed-timestamp">${timeStamp.toLocaleString()}</span> </div> </div>`);
+      } else {
+        alert("You say it best,\nwhen you say nothing at all.");
+      };
+      $(this).val("");
+    } 
+  });
+  
+  // press this button as an alternative way to reply for tablet and mobile devices
+  $(".fb-feed-wrapper").on("click", ".comment-send-button", function(e) {
+    let comment = $(this).prev().children(".comment-text").val()
+    e.preventDefault();
+    if (comment !== "" && comment !== "\n") {
+      $(this).prev().children().last().after(`<div class="replied-grid"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <div class="comment-content"> <div><span class="username">${nickname}</span> ${comment}</div> <span class="feed-timestamp">${timeStamp.toLocaleString()}</span> </div> </div>`);
+    } else {
+      alert("You say it best,\nwhen you say nothing at all.");
+    };
+    $(this).prev().children(".comment-text").val("");
+  })
 
 });
 
