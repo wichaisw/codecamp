1a357d5f10f6a27132fd800bd774142cca81c7b1
KradasA4 <wichai.sw19@gmail.com>
2020-02-29 20:22:46 +0700

    add like counter
    like counter based on blue-liked-button class toggle

--- jquery-hw/script.js
+++ jquery-hw/script.js
@@ -43,29 +43,15 @@ $(document).ready(function() {
       };
     }
   });
-
-  // like-button toggle
-  $(".fb-feed-wrapper").on("click", ".like-button", function() {
-    $(this).toggleClass("blue-liked-button");
-  });
-
-  // $(".like-button").on({
-  //   click: function() {
-  //     $(this).toggleClass("blue-liked-button");
-  //     alert('clicked!');
-  //   }
-  // });   
-
-
-  // add feed if #post-text iss not empty
+    
+    // add feed if #post-text iss not empty
   $(".post-button").click(function() {
     let timeStamp = new Date();
     
     let attachmentUrl = $("#image-video-attach-url").val();
     let postText = $("#post-text").val();
 
-    let htmlFeed = `<div class="fb-feed-template"> <div class="fb-feed-layout"> <div class="feed-header-grid fb-post-padding"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <div class="username-box"> <span class="username">${nickname}</span> <br> ${timeStamp.toLocaleString()} </div> </div> <div class="feed-content"> <div class="received-post">${postText}</div> <div class="received-attachment"> <img src="${attachmentUrl}" class="feed-img-attachment"> </div> </div> <div class="feed-engagement fb-post-padding"> <button class="like-button">Like</button> <span>0</span> </div> <div class="fb-post-padding"> <form class="fb-comment-grid"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <label for="comment-text"> <textarea name="comment-text" class="comment-text" rows="2" placeholder="Write a comment..."></textarea> </label> </form> </div> </div> </div>`
-
+    let htmlFeed = `<div class="fb-feed-template"> <div class="fb-feed-layout"> <div class="feed-header-grid fb-post-padding"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <div class="username-box"> <span class="username">${nickname}</span> <br> ${timeStamp.toLocaleString()} </div> </div> <div class="feed-content"> <div class="received-post">${postText}</div> <div class="received-attachment"> <img src="${attachmentUrl}" class="feed-img-attachment"> </div> </div> <div class="feed-engagement fb-post-padding"> <button class="like-button">Like</button> <span>${totalLike}</span> </div> <div class="fb-post-padding"> <form class="fb-comment-grid"> <img src="./img/anonymous.jpg" alt="user-pic-thumbnail" class="user-pic-thumbnail"> <label for="comment-text"> <textarea name="comment-text" class="comment-text" rows="2" placeholder="Write a comment..."></textarea> </label> </form> </div> </div> </div>`
     if (postText !== "" || attachmentUrl !== "") {
       $(".fb-feed-wrapper").css({display: "flex"});
       $(".fb-feed-wrapper").prepend(htmlFeed);
@@ -78,11 +64,24 @@ $(document).ready(function() {
     };
   });
 
+  // like-button toggle & totalLike counter based on a class toggle
+  let totalLike = 0;
+
+  $(".fb-feed-wrapper").on("click", ".like-button", function() {
+    $(this).toggleClass("blue-liked-button");
+    if ($(this).attr("class") == "like-button blue-liked-button") {
+      totalLike = totalLike + 1;
+      $(this).next().html(`${totalLike}`);
+    } else if ($(this).attr("class") == "like-button") {
+      totalLike = totalLike - 1;
+      $(this).next().html(`${totalLike}`);
+    };
+  });
+
   // press enter to reply
   $(".fb-feed-wrapper").on("keydown", ".comment-text", function(e) {
     let comment = $(this).val()
     let timeStamp = new Date();
-
       if (e.which == 13) {
         e.preventDefault();
         if (comment !== "" && comment !== "\n") {
