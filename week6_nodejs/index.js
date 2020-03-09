29ae74e62222c25b4e566b44b8fa00608ac4a70a
KradasA4 <wichai.sw19@gmail.com>
2020-03-09 11:55:47 +0700

    add module fs, write, read, append function,  callback function examples

--- week6_nodejs/index.js
+++ week6_nodejs/index.js
@@ -1,3 +1,47 @@
-const array = [1,2,3,5,5]
+// use module File System
+const fs = require("fs");
+console.log(typeof fs);
 
-console.log(array)
+// create codecamp.txt file, add data in the file
+// fs.writeFileSync("codecamp.txt","Hello, world!");
+// fs.appendFileSync('codecamp.txt', "Hello, Codecamp.")
+
+
+// let content = fs.readFileSync("codecamp.txt", "UTF-8");
+// console.log(content);
+// alternative way 
+// let content = fs.readFileSync("codecamp.txt", {
+//   encoding: "utf-8"
+// });
+// console.log(content);
+
+// add function
+function addSync(a, b, callbackFn) {
+  callbackFn(a + b);
+};
+
+addSync(2, 3, (result) => {
+  console.log(result);
+});
+
+// multiply function
+function mulSync(a, b, callback) {
+  callback(a*b);
+};
+
+mulSync(4, 6, function(multiplied) {
+  console.log(multiplied);
+});
+
+// max function
+function maxSync(a, b, callback) {
+  if (a > b) {
+    callback(a);
+  } else {
+    callback(b);
+  }
+};
+
+maxSync(4, 6, function(result) {
+  console.log(result);
+});
