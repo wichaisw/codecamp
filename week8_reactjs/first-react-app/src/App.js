add8f28a411a13fd72626be10224ea15dd42fde1
KradasA4 <wichai.sw19@gmail.com>
2020-03-23 16:30:20 +0700

    add variable, condition & function to App.js for practicing

--- week8_reactjs/first-react-app/src/App.js
+++ week8_reactjs/first-react-app/src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+let test1 = "monkey bobo"
+let myScore = Number(prompt("enter your score: "));
+// const gradeCalculator = (score) => {
+//   let grade;
+//   if (score >= 80 ) {
+//     grade = "A"
+//   } else if(score >= 70) {
+//     grade = "B"
+//   } else if(score >= 60) {
+//     grade = "C"
+//   } else if(score >= 50) {
+//     grade = "D"
+//   } else if (score < 50) {
+//     grade = "F"
+//   }
+//   return grade;
+// }
+const gradeCalculator = (score) => {
+  let grade = (score >= 80 && "A") || (score >= 70 && "B") || (score >= 60 && "C") || (score >= 50 && "D") || "F";
+  return grade;
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Try Me!
-        </a>
-      </header>
+    <div>
+          <p style={ {fontSize:"50px"} } className="App__text--color App__text--bgcolor">Hello, Moto</p>
+          <p>Hi {test1}</p>
+
+          Your score is {myScore}. You got an {gradeCalculator( myScore ) } grade. 
+          {/* Your grade is: {grade} */}
     </div>
   );
 }
