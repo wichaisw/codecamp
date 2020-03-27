109926a8cb287c578500807eff58bcbfedc3e8d8
KradasA4 <wichai.sw19@gmail.com>
2020-03-27 15:20:50 +0700

    add layout to MyWeb.js using antD framework

--- week8_reactjs/20200327-react_router/src/Homepage.js
+++ week8_reactjs/20200327-react_router/src/Homepage.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import 'antd/dist/antd.css';
+import './Homepage.css';
+import { Button, Layout } from 'antd';
+import { Row, Col, Divider } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 export default class Homepage extends Component {
   render() {
     return (
-      <div>
-        <h1> This is homepage</h1>
-        <Link to="./resume">Resume</Link><br/>
-        <Link to="./app">App</Link><br/>
-        <Link to="./facebook">Facebook</Link><br/>
-      </div>
+      <>
+        <Layout>
+            <Header><h1> This is homepage</h1></Header>
+          <Layout>
+              <Sider><Link to="./app">App</Link></Sider>
+            <Content>
+              <Link to="./resume">Resume</Link><br />
+              <Button type="primary" className="button"> <Link to="./facebook">Facebook</Link><br /> </Button>
+            </Content>
+          </Layout> 
+        </Layout>
+      </>
     )
   }
 }
