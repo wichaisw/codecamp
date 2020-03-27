03c26c7d6c4f123cb08fe475f95538200a0332d6
KradasA4 <wichai.sw19@gmail.com>
2020-03-27 11:34:13 +0700

    create react router folders for practicing

---
+++ week8_reactjs/20200327-react_router/src/Facebook.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Facebook extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is facebook page</h1>
+      </div>
+    )
+  }
+}
---
+++ week8_reactjs/20200327-react_router/src/MyWeb.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom';
+import Homepage from './Homepage';
+import Resume from './Resume';
+import Facebook from './Facebook';
+import App from './App';
+export default class MyWeb extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Homepage } />
+        <Route path="/resume" component={ Resume } />
+        <Route path="/facebook" component={ Facebook } />
+        <Route path="/app" component={ App } />
+      </Switch>
+
+
+    )
+  }
+}
