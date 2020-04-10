f8716a524492d18b072cc1ee72dcecf8a522c518
KradasA4 <wichai.sw19@gmail.com>
2020-04-10 12:20:09 +0700

    add navbar logo

--- week9_reactjs/20200407_facebook-page/src/components/HomePage/HomeNav.js
+++ week9_reactjs/20200407_facebook-page/src/components/HomePage/HomeNav.js
@@ -6,7 +6,12 @@ export default function HomeNav() {
   return (
     <Row className="home-nav">
       <Col>
-        <h1>FUCK</h1>
+        <div className="home-nav__logo _19eb" data-gt='{"chrome_nav_item":"logo_chrome"}' href="https://www.facebook.com/?ref=logo">
+          <span className=" _2md"></span>
+        </div>
+      </Col>
+      <Col className="home-nav__search-box">
+        <input type="text" className="home-nav__search-input" />
       </Col>
       <Col></Col>
     </Row>
