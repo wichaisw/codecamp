3fae0ab123e4ac389a6d8b80828f8b4c1be96605
KradasA4 <wichai.sw19@gmail.com>
2020-05-04 20:05:04 +0700

    create user model & route

---
+++ recap/basic-backend/models/user.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, Datatypes) => {
+  const User = sequelize.define('User', 
+    {
+      username: {
+        type: Datatypes.STRING,
+        unique: true
+      },
+      password: {
+        type: Datatypes.STRING,
+      },
+    }, 
+    {
+    tableName: 'users',
+    timestamps: false,
+    }
+  )
+
+  return User
+};
---
+++ recap/basic-backend/routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user');
+
+router.get('/', userController.getAllUser);
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+
+module.exports = router;
