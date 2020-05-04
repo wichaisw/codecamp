f34d09b5b38d90b3dd9ee2ea4b8c8c3c41c1bc06
KradasA4 <wichai.sw19@gmail.com>
2020-05-04 20:05:21 +0700

    add authentication

--- recap/basic-backend/routes/student.js
+++ recap/basic-backend/routes/student.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/student.js')
+const passport = require('passport')
 
-module.exports = router;
+const auth = passport.authenticate('jwt-authentication', {session: false});
 
 router.get('/', studentController.getAllStudents);
 router.get('/:id', studentController.getStudentById);
 router.post('/', studentController.createNewStudent);
 router.put('/:id', studentController.editStudentById);
-router.delete('/:id', studentController.deleteStudentById);
+router.delete('/:id', auth, studentController.deleteStudentById);
+
+module.exports = router;
